476c8f2b58a97fc7dd353c3122472d96db156318
anjalika22 <74655570+anjalika22@users.noreply.github.com>
2022-09-29 13:55:04 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from  'react-dom';
+ 
+const head="Heading";
+const fname="Template Literal";
 
 
 ReactDOM.render(
-  <>//REACT FRAGMENT helps to render multiple jsx elements+helps to not create an extra node
-  <h1>hello world!<h2>
-  <p>hey<p>, document.getElementById("root")
-);
-
-
-
+<>
+<h1> {head + " " +fname}</h1> 
+<h2>My name is {fname} {head}</h2>
+<h2>Javascript Expression {Math.random()}</h2> 
+<p>my lucky number is {2+3} </p>
+  <ol>
+    <li>Dark</li>
+    <li>stranger</li>
+  </ol>
+  </>,document.getElementById("root")
+  );
